1f094d02d62581fc8cc862f9b9c3013299a2f3c8
Nico Pinzel <57192862+nicopinzel@users.noreply.github.com>
2021-05-10 19:18:03 +0200

    login message
    shows message which tells you that the add-on loaded succesful

--- MaxPlaytime.lua
+++ MaxPlaytime.lua
@@ -1,1 +1,3 @@
-print("1")
+print("MaxPlaytime - Collects the total playtime of all of your characters except deleted ones.")
+print("This AddOn isn´t workin yet.")
+print("AddOn loaded succesfully!")
